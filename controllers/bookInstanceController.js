2a816babc6c54363836c17f61e5c16cb7e3e82e0
Miguel  de la Mata <sebidelamata@gmail.com>
2024-01-25 11:16:27 -0700

    syntax error

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,4 +1,4 @@
-const BookInstance = require("../models/bookinstance");
+const BookInstance = require("../models/bookInstance");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult} = require('express-validator')
 const Book = require('../models/book')
